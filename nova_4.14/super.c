fc0463ade8416422cc50f86bba7927c3e8580c51
李溶燮(Lee, Yongseob) <Yongseob.Rhee@gmail.com>
2018-11-22 18:28:39 +0900

    dax_direct_access

--- nova_4.14/super.c
+++ nova_4.14/super.c
@@ -137,8 +137,11 @@ static int nova_get_nvmm_info(struct super_block *sb,
 	}
 	sbi->s_dax_dev = dax_dev;
 
-	size = dax_direct_access(sbi->s_dax_dev, 0, LONG_MAX/PAGE_SIZE,
-				 &virt_addr, &__pfn_t) * PAGE_SIZE;
+	//size = dax_direct_access(sbi->s_dax_dev, 0, LONG_MAX/PAGE_SIZE,
+	//			 &virt_addr, &__pfn_t) * PAGE_SIZE;
+	size = dax_direct_access(dax_dev, 0, LONG_MAX/PAGE_SIZE,
+			&virt_addr, &__pfn_t); //v 4.14.
+	size = memblock.pram.total_size;//bytes
 	if (size <= 0) {
 		nova_err(sb, "direct_access failed\n");
 		return -EINVAL;
