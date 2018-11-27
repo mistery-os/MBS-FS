fad0248e0f773ae50a03e7bc82dc9619eedf16ce
李溶燮(Lee, Yongseob) <Yongseob.Rhee@gmail.com>
2018-11-27 15:42:59 +0900

    vmalloc_pram

--- mbs_brd/mbs.c
+++ mbs_brd/mbs.c
@@ -406,7 +406,7 @@ static long __mbs_direct_access(struct mbs_device *mbs, pgoff_t pgoff,
 	*pfn = page_to_pfn_t(page);
 #endif
 	mbs_size = memblock.pram.total_size/PAGE_SIZE;// bytes
-	vm = vmalloc(mbs_size);
+	vm = vmalloc_pram(mbs_size);//vm = vmalloc(mbs_size);
 	page=vmalloc_to_page(vm);
 	//*kaddr = page_address(vmalloc_to_page(vm));
 	//*pfn = (pfn_t)vmalloc_to_pfn(vm);
