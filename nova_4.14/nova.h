40228833e8021ee06f948be942fce6069726df78
李溶燮(Lee, Yongseob) <Yongseob.Rhee@gmail.com>
2018-12-14 16:53:33 +0900

    //nova_snapshot_cleaner_init

--- nova_4.14/nova.h
+++ nova_4.14/nova.h
@@ -325,7 +325,7 @@ if (nid == 0)
 	return block ? ((void *)ps + block) : NULL;
 else
 #endif
-	nova_info("%s: virt_addr + block = 0x%lx\n",(unsigned long)(sbi->virt_addr[nid] + block));
+	nova_info("%s: virt_addr + block = 0x%llx\n",(unsigned long)(sbi->virt_addr[nid] + block));
 	return block ? ((void *)(sbi->virt_addr[nid] + block) ) : NULL;
 }
 static inline void *nova_get_block(struct super_block *sb, u64 block)
