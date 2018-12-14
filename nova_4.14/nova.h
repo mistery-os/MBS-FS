d2d5d362dd5d4591004ef2fbeb59a92472c57b1f
李溶燮(Lee, Yongseob) <Yongseob.Rhee@gmail.com>
2018-12-14 16:45:17 +0900

    //nova_snapshot_cleaner_init

--- nova_4.14/nova.h
+++ nova_4.14/nova.h
@@ -319,10 +319,12 @@ static inline void *nova_get_block_regions(struct super_block *sb, u64 block,
 		int nid)
 {
 	struct nova_sb_info *sbi = NOVA_SB(sb);
+#if 0
 	struct nova_super_block *ps = nova_get_super_regions(sb, nid);
 if (nid == 0)
 	return block ? ((void *)ps + block) : NULL;
 else
+#endif
 	return block ? ( sbi->virt_addr[nid] + block ) : NULL;
 }
 static inline void *nova_get_block(struct super_block *sb, u64 block)
