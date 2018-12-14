4173b7c0a3bcfa01e5ca9eb17f932a5b465c518a
李溶燮(Lee, Yongseob) <Yongseob.Rhee@gmail.com>
2018-12-14 12:57:44 +0900

    //nova_snapshot_cleaner_init

--- nova_4.14/mprotect.h
+++ nova_4.14/mprotect.h
@@ -47,7 +47,7 @@ nova_info("%s: where am I, <<<<<<<<HERE suspect====== HERE  == \n",__func__);
 			(unsigned long)p, (unsigned long)len);
 		
 		for(i=0;i<memblock.pram.cnt;i++){
-			nova_err(" sbi->virt_addr[%d]= 0x%lx, size=  0x%lx\n",
+			nova_err(sb, " sbi->virt_addr[%d]= 0x%lx, size=  0x%lx\n",
 				sbi->virt_addr[i],memblock.pram.regions[i].size);
 		}
 
